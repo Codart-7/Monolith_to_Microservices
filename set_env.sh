d597329828f303cedd1b6b7b92d459cfebfb678f
Codart-7 <iconmoa@gmail.com>
2022-09-15 15:58:20 +0100

    [update] init

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=superdev1
+export POSTGRES_PASSWORD=superdev1
+export POSTGRES_HOST=superdev1.ctmuiz2sdnmx.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=superdev1
+export AWS_BUCKET=superdev1
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
